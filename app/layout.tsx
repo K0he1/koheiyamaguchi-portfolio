febeb9f2a5d960d38e2c40ae263bf9a7af77528a
Kohei Yamaguchi <fp74skzcz@gmail.com>
2026-08-14 19:15:20 +0900

    Update metadata title and description for clarity and consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Kohei Yamaguchi | Portfolio",
-  description: "Kohei Yamaguchiのプロフィールと制作物を紹介するポートフォリオサイトです．",
+  title: "K's Home Page",
+  description: "K's Home Pageは、Kohei Yamaguchiのプロフィールや制作物を紹介し、お問い合わせを受け付けるポートフォリオサイトです。",
 };
 
 export default function RootLayout({
